848668bae193d274d34ef0544ce2432fffa358b3
陈恩湖 <chenenhu@ifanr.com>
2017-04-16 00:10:48 +0800

    遗漏内容的补充提交

--- ActionSheetView/ActionSheetView/ActionSheetViewController.swift
+++ ActionSheetView/ActionSheetView/ActionSheetViewController.swift
@@ -6,14 +6,23 @@
 //  Copyright © 2017年 Judson. All rights reserved.
 //
 
+private extension Selector {
+static let clickProfileCenterActionSheetItem = #selector(ActionSheetViewController.clickActionSheetItem(_:))
+
+}
+
+
 import UIKit
 
-class ViewController: UIViewController {
+class ActionSheetViewController: UIViewController {
     
-    @IBOutlet weak var bottomSheetButton: UIButton!
+    @IBOutlet weak var bottomSheetButton1: UIButton!
+    @IBOutlet weak var bottomSheetButton2: UIButton!
+    @IBOutlet weak var bottomSheetButton3: UIButton!
+    @IBOutlet weak var bottomSheetButton4: UIButton!
+    @IBOutlet weak var bottomSheetButton5: UIButton!
+    @IBOutlet weak var bottomSheetButton6: UIButton!
 
-    @IBOutlet weak var centerHintButton: UIButton!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,31 +31,105 @@ class ViewController: UIViewController {
     
     fileprivate func initializeSubViews() {
         // 初始化显示控件
-        bottomSheetButton.layer.borderWidth = 1
-        bottomSheetButton.layer.borderColor = UIColor.gray.cgColor
-        bottomSheetButton.layer.cornerRadius = 2
-        bottomSheetButton.clipsToBounds = true
+        let buttons: [UIButton] = [bottomSheetButton1, bottomSheetButton2, bottomSheetButton3, bottomSheetButton4, bottomSheetButton5, bottomSheetButton6]
+        initializeButtons(buttons: buttons)
         
-        // 初始化显示控件
-        centerHintButton.layer.borderWidth = 1
-        centerHintButton.layer.borderColor = UIColor.gray.cgColor
-        centerHintButton.layer.cornerRadius = 2
-        centerHintButton.clipsToBounds = true
     }
     
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-
+    fileprivate func initializeButtons(buttons: [UIButton]) {
+        for (index, object) in buttons.enumerated() {
+            object.layer.borderWidth = 1
+            object.layer.borderColor = UIColor.gray.cgColor
+            object.layer.cornerRadius = 2
+            object.clipsToBounds = true
+            object.tag = index // 为 button 指定一个 tag （from 0 to 5）
+        }
     }
     
 
     @IBAction func showHintViewAction(_ sender: UIButton) {
-        print("显示点什么东西--\(sender)")
+        
+        switch sender.tag {
+            case 0:
+                showEditMenuActionSheet(actionType: .editProfileCenter)
+                
+            case 1:
+                showEditMenuActionSheet(actionType: .handledImageRecognizer)
+
+            case 2:
+                showEditMenuActionSheet(actionType: .sharedOtherRecommendComment)
+                
+            case 3:
+                showEditMenuActionSheet(actionType: .handledComment)
+
+            case 4:
+                showEditMenuActionSheet(actionType: .profileEditPhotoToken)
+
+            case 5:
+                showEditMenuActionSheet(actionType: .setting)
+            
+            default:
+                break
+        }
     }
     
+    fileprivate func showEditMenuActionSheet(actionType: SourceControlType) {
+        NotificationCenter.default.addObserver(self, selector: .clickProfileCenterActionSheetItem, name: NSNotification.Name(rawValue: "ActionSheetClickedNotification"), object: nil)
+        
+        let actionSheetView = BottomActionSheetView()
+        actionSheetView.initialSheetViewItems(actionType)
+        actionSheetView.show()
+        actionSheetView.clickBackgroundGrayViewAction = { [weak self] in
+            guard let strongSelf = self else {
+                return
+            }
+            NotificationCenter.default.removeObserver(strongSelf, name: NSNotification.Name(rawValue: "ActionSheetClickedNotification"), object: nil)
+        }
+    }
     
-    
+    @objc fileprivate func clickActionSheetItem(_ notification: NSNotification) {
+        guard let notificationObject = notification.object as? ActionSheetItemType else {
+            return
+        }
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name(rawValue: "ActionSheetClickedNotification"), object: nil)
+        switch notificationObject {
+        case .edit:
+            print("Edit Action")
+            
+        case .delete:
+            print("Delete Action")
+        case .recognizedPhoto:
+            print("recognizedPhoto Action")
+
+        case .exitLogin:
+            print("exitLogin Action")
+
+        case .tokenPhotos:
+            print("exitLogin Action")
+
+        case .chosenFromAlbum:
+            print("chosenFromAlbum Action")
+
+        case .reply:
+            print("reply Action")
+
+        case .report:
+            print("report Action")
+
+        case .share:
+            print("share Action")
+
+        case .savedPhotos:
+            print("savedPhotos Action")
+
+        case .copy:
+            print("copy Action")
+
+        case .cancel:
+            print("cancel Action")
+            
+        }
+    }
 
 }
 
--- ActionSheetView/ActionSheetView/BottomActionSheetView.swift
+++ ActionSheetView/ActionSheetView/BottomActionSheetView.swift
@@ -6,4 +6,244 @@
 //  Copyright © 2017年 Judson. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+private extension Selector {
+    static let clickBackgroundGrayView = #selector(BottomActionSheetView.clickBackgroundGrayView)
+    static let clickActionSheetItem = #selector(BottomActionSheetView.clickActionSheetItem(_:))
+    
+}
+
+enum SourceControlType {
+    case setting
+    case profileEditPhotoToken
+    case deletedComment
+    case handledComment
+    case handledProfileRecommendComment
+    case sharedOtherRecommendComment
+    case handledArticlePhotos
+    case handledImageRecognizer
+    case editProfileCenter
+}
+
+enum ActionSheetItemType: String {
+    case recognizedPhoto = "识别二维码或链接"
+    case exitLogin = "退出登录"
+    case tokenPhotos = "拍照"
+    case chosenFromAlbum = "从手机相册选取"
+    case delete = "删除"
+    case reply = "回复"
+    case report = "举报"
+    case share = "分享"
+    case edit = "编辑"
+    case savedPhotos = "存储图片"
+    case copy = "拷贝"
+    case cancel = "取消"
+}
+
+let screenWidth = UIScreen.main.bounds.width
+let screenHeight = UIScreen.main.bounds.height
+
+class BottomActionSheetView: UIView {
+    
+    fileprivate var contentViewHeight: CGFloat = 0
+    fileprivate var backgroundGrayView: UIView!
+    fileprivate var contentView: UIView!
+    fileprivate var cancelButton: UIButton!
+    fileprivate var blackTextColor: UIColor = UIColor.black
+    fileprivate var redTextColor: UIColor = UIColor.red
+    fileprivate var grayTextColor: UIColor = UIColor.gray
+    fileprivate var actionItemType: ActionSheetItemType = .cancel
+    
+    var itemHeight: CGFloat = 60
+    
+    var clickBackgroundGrayViewAction: (() -> Void)?
+    
+    init() {
+        super.init(frame: CGRect(x: 0, y: 0, width: screenWidth, height: screenHeight))
+        self.alpha = 1.0
+        
+        backgroundGrayView = UIView()
+        backgroundGrayView.frame = self.bounds
+        backgroundGrayView.backgroundColor = UIColor.black
+        backgroundGrayView.alpha = 0.4
+        backgroundGrayView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: .clickBackgroundGrayView))
+        addSubview(backgroundGrayView)
+        
+        // 除了取消按钮之外的 内容视图
+        contentView = UIView()
+        contentView.frame = CGRect(x: 0, y: screenHeight, width: frame.size.width, height: itemHeight)
+        contentView.backgroundColor = UIColor.white
+        insertSubview(contentView, aboveSubview: backgroundGrayView)
+        
+        // 取消按钮
+        let cancelText = NSLocalizedString("ActionSheet_Cancel", comment: "底部弹框取消按钮文本")
+        cancelButton = UIButton(type: .custom)
+        cancelButton.frame = CGRect(x: 0, y: contentView.bounds.size.height - itemHeight, width: contentView.bounds.size.width, height: itemHeight)
+        cancelButton.setTitle(cancelText, for: .normal)
+        cancelButton.addTarget(self, action: .clickActionSheetItem, for: .touchUpInside)
+        cancelButton.setTitleColor(grayTextColor, for: .normal)
+        cancelButton.alpha = 0.8
+    }
+    
+    func initialSheetViewItems(_ sourceControlType: SourceControlType, compeltionHandler: ((Bool) -> Void)? = nil) {
+        
+        switch sourceControlType {
+        case .setting:
+            let exitLoginText: String = NSLocalizedString("ActionSheet_ExitLogin", comment: "退出登录文本")
+            addActionSheetItems([exitLoginText], textColor: redTextColor)
+            
+        case .profileEditPhotoToken:
+            let tokenPhotoText: String = NSLocalizedString("ActionSheet_TokenPhoto", comment: "拍照文本")
+            let fetchPhotosFromAlbumText: String = NSLocalizedString("ActionSheet_FetchPhotosFromAlbum", comment: "从手机相册选取文本")
+            addActionSheetItems([tokenPhotoText, fetchPhotosFromAlbumText], textColor: blackTextColor)
+            
+        case .deletedComment:
+            let deleteText: String = NSLocalizedString("ActionSheet_Delete", comment: "删除文本")
+            addActionSheetItems([deleteText], textColor: redTextColor)
+            
+        case .handledComment:
+            let replyText: String = NSLocalizedString("ActionSheet_Reply", comment: "回复文本")
+            let reportText: String = NSLocalizedString("ActionSheet_Report", comment: "举报文本")
+            addActionSheetItems([replyText, reportText], textColor: blackTextColor)
+            
+        case .sharedOtherRecommendComment:
+            let shareText: String = NSLocalizedString("ActionSheet_Share", comment: "分享文本")
+            addActionSheetItems([shareText], textColor: blackTextColor)
+            
+        case .handledProfileRecommendComment:
+            let editText: String = NSLocalizedString("ActionSheet_Edit", comment: "编辑文本")
+            let shareText: String = NSLocalizedString("ActionSheet_Share", comment: "分享文本")
+            let deleteText: String = NSLocalizedString("ActionSheet_Delete", comment: "删除文本")
+            addActionSheetItems([editText, shareText, deleteText], textColor: blackTextColor)
+            
+        case .handledArticlePhotos:
+            let savePhotoText: String = NSLocalizedString("ActionSheet_Image_Save", comment: "存储图片文本")
+            let pasteText: String = NSLocalizedString("ActionSheet_ImageURL_Paste", comment: "拷贝文本")
+            addActionSheetItems([savePhotoText, pasteText], textColor: blackTextColor)
+            
+        case .handledImageRecognizer:
+            let imageRecognizedText: String = NSLocalizedString("ActionSheet_Image_Recognize", comment: "识别二维码或链接")
+            let savePhotoText: String = NSLocalizedString("ActionSheet_Image_Save", comment: "存储图片文本")
+            let pasteText: String = NSLocalizedString("ActionSheet_ImageURL_Paste", comment: "拷贝文本")
+            addActionSheetItems([imageRecognizedText, savePhotoText, pasteText], textColor: blackTextColor)
+            
+        case .editProfileCenter:
+            let editText: String = NSLocalizedString("ActionSheet_Edit", comment: "编辑文本")
+            let deleteText: String = NSLocalizedString("ActionSheet_Delete", comment: "删除文本")
+            addActionSheetItems([editText, deleteText], textColor: blackTextColor)
+            
+        }
+        
+        // add cancel button
+        cancelButton.frame = CGRect(x: 0, y: contentView.bounds.size.height - itemHeight, width: contentView.bounds.size.width, height: itemHeight)
+        contentView.addSubview(cancelButton)
+        // 判断是否初始化成功
+        compeltionHandler?(contentViewHeight != 0)
+    }
+    
+    fileprivate func addActionSheetItems(_ itemsTitle: [String], textColor: UIColor) {
+        let itemCount = itemsTitle.count + 1
+        contentViewHeight = CGFloat(itemCount) * itemHeight
+        contentView.frame = CGRect(x: 0, y: screenHeight, width: frame.size.width, height: contentViewHeight)
+        for (index, title) in itemsTitle.enumerated() {
+            initialItemButton(index: index, text: title, titleColor: textColor)
+        }
+    }
+    
+    fileprivate func initialItemButton(index: Int, text: String, titleColor: UIColor) {
+        let button = UIButton(type: .custom)
+        button.setTitle(text, for: .normal)
+        button.titleLabel?.textAlignment = .center
+        button.frame = CGRect(x: 0, y: itemHeight * CGFloat(index), width: contentView.bounds.size.width, height: itemHeight)
+        button.addTarget(self, action: .clickActionSheetItem, for: .touchUpInside)
+        button.setTitleColor(titleColor, for: .normal)
+        contentView.addSubview(button)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func show() {
+        guard contentViewHeight != 0 else {
+            print("error: 必须先初始化 contentView,再调用显示方法")
+            return
+        }
+        UIApplication.shared.keyWindow?.addSubview(self)
+        UIApplication.shared.keyWindow?.bringSubview(toFront: self)
+        self.backgroundGrayView.alpha = 0.0
+        UIView.animate(withDuration: 0.4, animations: {
+            self.backgroundGrayView.alpha = 0.4
+            self.contentView.frame.origin.y -= self.contentViewHeight
+        })
+    }
+    
+}
+
+// MARK: 点击方法
+extension BottomActionSheetView {
+    
+    @objc fileprivate func clickActionSheetItem(_ button: UIButton) {
+        guard let titleLabel = button.titleLabel, let titleText = titleLabel.text else {
+            return
+        }
+        hideSheetView()
+        switch titleText {
+        case "识别二维码或链接":
+            actionItemType = .recognizedPhoto
+            
+        case "退出登录":
+            actionItemType = .exitLogin
+            
+        case "拍照":
+            actionItemType = .tokenPhotos
+            
+        case "从手机相册选取":
+            actionItemType = .chosenFromAlbum
+            
+        case "删除":
+            actionItemType = .delete
+            
+        case "回复":
+            actionItemType = .reply
+            
+        case "举报":
+            actionItemType = .report
+            
+        case "分享":
+            actionItemType = .share
+            
+        case "编辑":
+            actionItemType = .edit
+            
+        case "存储图片":
+            actionItemType = .savedPhotos
+            
+        case "拷贝":
+            actionItemType = .copy
+            
+        case "取消":
+            actionItemType = .cancel
+            
+        default:
+            break
+        }
+        // 添加通知 监听点击事件
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "ActionSheetClickedNotification"), object: actionItemType)
+    }
+    
+    fileprivate func hideSheetView() {
+        UIView.animate(withDuration: 0.4, animations: {
+            self.backgroundGrayView.alpha = 0.0
+            self.contentView.frame.origin.y = self.frame.size.height
+        }, completion: { result in
+            self.removeFromSuperview()
+        })
+    }
+    
+    @objc fileprivate func clickBackgroundGrayView() {
+        clickBackgroundGrayViewAction?()
+        hideSheetView()
+    }
+}
